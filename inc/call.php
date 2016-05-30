32e398eb437fac94fa0c1d11e2ae9020ab5beb89
5Y2V3573R <sylvestersiani@5y2v3573r.home>
2016-05-30 21:48:39 +0100

    Revert "Tracking"
    This reverts commit 6cca85ec8ea50f1bf38609b4b7014455ef94d2ce.

--- inc/call.php
+++
@@ -1,3 +0,0 @@
-<?php  	require_once('inc/verify_login.php'); // checking if user is already logged in
-		require_once('inc/func/functions.php');	// including all necessary functions
-?>
--- inc/nav.php
+++
@@ -1,7 +0,0 @@
-<nav>
-	<ul id="nav-menu">
-		<li><a href="dashboard.php">Dashboard</a></li>
-		<li><a href="add.php">New Order</a></li>
-		<li><a href="inc/logout.php">Logout</a></li>
-	</ul>
-</nav>
